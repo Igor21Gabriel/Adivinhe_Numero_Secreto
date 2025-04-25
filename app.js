9938effa72f05d6ab69aa372622a671ec6bfd210
Igor21Gabriel <117647045+Igor21Gabriel@users.noreply.github.com>
2025-04-25 11:37:06 -0300

    alterando limite para 50

--- app.js
+++ app.js
@@ -25,11 +25,9 @@ while(chute != numeroSecreto){
 }
 */
 
-
-
 //versao Aprimorada
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -43,7 +41,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
   exibirTextoNaTela ('h1', 'Jogo do número secreto');
-      exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+      exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 exibirMensagemInicial();
 
